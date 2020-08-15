2df6f65d5e93defd5b0a9fd9b372e6d6d5dad708
Rob Kellen <rob.h.kellen@gmail.com>
2020-08-14 23:38:55 -0700

    finished product with interval for refresh of background color, set local storage, and updated styling

--- script.js
+++ script.js
@@ -1,73 +1,71 @@
 //access local storage
-function getLocalStorage(key){
+function getLocalStorage(key) {
   const value = localStorage.getItem(key);
   if (value) {
-    $("#text${key}").text(value);
+    $(`#text${key}`).text(value);
   }
-
 }
 
 //execute the following function after page is fully loaded
-$(document).ready(function() {
-//current day is displayed at the top of the calendar
-$("#currentDay").text(moment().format("dddd, MMMM Do"));
+$(document).ready(function () {
+  //current day is displayed at the top of the calendar
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
+  //for loop to create schedule content
+  for (let i = 9; i < 18; i++) {
+    //create new rows for each hour
+    const row = $(`<div data-time=${i} id='{i}' class="row">`);
 
-//for loop to create schedule content
-for (let i = 9; i < 18; i++) {
-  //create new rows for each hour
-  const row = $(`<div data-time=${i} id='${i}' class="row">`);
+    //create new column to display the hour for each row
+    const column1 = $(
+      "<div id='column1' class='col-sm-2'> <p class='hour'>" + formatAMPM(i) + "</p>"
+    );
+    //create new column for text area to hold event schedule
+    const column2 = $(
+      `<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`
+    );
+    //create new column to display save buttons for each row
+    const column3 = $(
+      `<div id='column3' class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`
+    );
 
-  //create new column to display the hour for each row
-  const column1 = $("<div class='col-sm-2'> <p class='hour'>" + formatAMPM(i) + "</p>");
-  //create new column for text area to hold event schedule
-  const column2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);    
-  //create new column to display save buttons for each row
-  const column3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
+    //appending the columns to the rows
+    row.append(column1);
+    row.append(column2);
+    row.append(column3);
+    //appending the rows to the HTML page
+    $(".container").append(row);
+    //accessing local storage to obtain current time
+    getLocalStorage(i);
+  }
+  //to format time block to read 'am' for morning hours or 'pm' for afternoon hours
+  function formatAMPM(hours) {
+    const ampm = hours >= 12 ? "pm" : "am";
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    return hours + ampm;
+  }
+  formatAMPM();
+  //change background color based on current time for user's location
+  function backgroundColors() {
+    const currentTime = new Date().getHours();
+    for (let i = 0; i < array.length; i++) {
+      if ($(`#${i}`).data("time") == currentTime) {
+        $(`#text${i}`).addClass("present");
+      } else if (currentTime < $(`#${i}`).data("time")) {
+        $(`#text${i}`).addClass("future");
+      }
+    }
+  }
+  //set interval to refresh background colors every 5 minutes based on the time at user's location
+  setInterval(function () {
+    updateColors();
+  }, 300000);
 
-  //appending the columns to the rows
-  row.append(column1);
-  row.append(column2);
-  row.append(column3);
-//appending the rows to the HTML page
-  $(".container").append(row);
-//accessing local storage to obtain current time
-  getLocalStorage(i);
-}
-
-function formatAMPM(hours) {
-  var ampm = hours >= 12 ? 'pm' : 'am';
-  hours = hours % 12;
-  hours = hours ? hours : 12;
-  return hours + ampm;
-}
-formatAMPM();
-   
+  var saveEntryBtn = $(".saveBtn");
+  saveEntryBtn.on("click", function () {
+    let eventId = $(this).attr("id");
+    let eventText = $(this).parent().siblings().children(".description").val();
+    localStorage.setItem(eventId, eventText);
+  });
 });
-
-// var time = new Date().getHours();
-// if (time >= 12) {
-//   time = time + "pm";
-// } else if (time / 12 === 1) {
-//   time = 12 + "pm"
-// } else {
-//   time = time + "am"
-// };
-// return time;
-
-
-
-
-  
-  
-  
-
-
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
